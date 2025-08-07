af1498501e257d54491d7c69050e196cd9383c41
Nora Beda <theo.github@bedafamily.com>
2025-08-07 11:27:36 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+ARG IMAGE=debian:latest
+FROM --platform=$BUILDPLATFORM ${IMAGE} AS build
+SHELL [ "/bin/bash", "-c" ]
+
+WORKDIR /robot-util
+COPY scripts scripts
+
+ARG TARGETOS
+ARG TARGETARCH
+ARG BUILDARCH
+
+RUN scripts/buildenv.sh ${TARGETOS} ${TARGETARCH} ${BUILDARCH}
+
+COPY CMakeLists.txt .
+COPY src src
+
+RUN scripts/build.sh ${TARGETOS} ${TARGETARCH}
+
+FROM ${IMAGE} AS runtime
+SHELL [ "/bin/bash", "-c" ]
+WORKDIR /robot-util
+
+COPY --from=build /robot-util/build ./build
+
+LABEL org.opencontainers.image.source=https://github.com/heyallnorahere/robot-util
+LABEL org.opencontainers.image.licenses=Apache-2.0
+
+ENTRYPOINT [ "/robot-util/build/src/robot-util" ]
---
+++ scripts/buildenv.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OS=$1
+TARGETARCH=$2
+BUILDARCH=$3
+
+apt-get update
+apt-get install -y jq cmake build-essential
+
+if [[ $TARGETARCH != $BUILDARCH ]]; then
+    SCRIPTDIR=$(realpath $(dirname $0))
+    PLATFORMS_JSON="$SCRIPTDIR/platforms.json"
+
+    COMPILEROS_JSONPATH=".$OS.Name"
+    COMPILEROS=$(cat $PLATFORMS_JSON | jq -r $COMPILEROS_JSONPATH)
+
+    COMPILERARCH_JSONPATH=".$OS.Architectures.$TARGETARCH"
+    COMPILERARCH=$(cat $PLATFORMS_JSON | jq -r $JSONPATH)
+
+    COMPILERNAME=$COMPILERARCH-$COMPILEROS
+    apt-get install -y gcc-$COMPILERNAME binutils-$COMPILERNAME
+
+    dpkg --add-architecture $TARGETARCH
+    apt-get update
+fi
+
+apt-get install -y libcurl4-openssl-dev:$TARGETARCH
